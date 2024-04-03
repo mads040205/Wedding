8cb5f7c0a69202ceef3f59254cd02016fe7b52fd
mads020405 <maddie040205@gmail.com>
2024-04-02 20:44:27 -0400

    Update Wedding Party and add Dirs

--- Wedding Party/publish.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp -v index.html style.css ~/.config/www/neveee.com/Resume
-cp -v index.html style.css ~/.config/www/neveee.com/resume
-cp -v index.html style.css ~/.config/www/neveee.com/Resume-Insurance
-echo "Done!"
--- Wedding Party/run.sh
+++
@@ -1,1 +0,0 @@
-concurrently "tailwindcss -i input.css -o style.css --watch"  "live-server --port=3000"
